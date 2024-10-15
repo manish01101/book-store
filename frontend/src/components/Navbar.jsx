95924eccbde4844df0a7c7c5c35c82a17def50b9
manish01101 <manishchotu4@gmail.com>
2024-10-15 14:35:49 +0530

    navbar created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineBars } from "react-icons/ai";
+import { FaSearch } from "react-icons/fa";
+import { HiMiniUserCircle } from "react-icons/hi2";
+import { HiOutlineHeart } from "react-icons/hi";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+
+const navigation = [
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Orders", href: "/orders" },
+  { name: "Cart Page", href: "/cart" },
+  { name: "Check OUt", href: "/checkout" },
+];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const currentUser = "manish";
 
-export default Navbar
+  return (
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        {/* left side */}
+        <div className="flex justify-between items-center gap-4 md:gap-16">
+          {/* logo */}
+          <Link to="/">
+            <AiOutlineBars className="size-6" />
+          </Link>
+
+          {/* search */}
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <FaSearch className="absolute inline-block left-3 inset-y-2" />
+            <input
+              type="text"
+              placeholder="Search here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="relative flex items-center md:space-x-3 space-x-2 ">
+          <div>
+            {currentUser ? (
+              <>
+                <button
+                  className="size-7 font-bold bg-primary rounded-full"
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                  {currentUser[0].toUpperCase()}
+                </button>
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
+                      {navigation.map((item) => (
+                        <li
+                          key={item.name}
+                          onClick={() => setIsDropdownOpen(false)}
+                        >
+                          <Link
+                            to={item.href}
+                            className="block px-4 py-2 text-sm hover:bg-gray-100"
+                          >
+                            {item.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Link to={"/login"}>
+                <HiMiniUserCircle className="size-6" />
+              </Link>
+            )}
+          </div>
+
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6" />
+          </button>
+
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm"
+          >
+            <HiOutlineShoppingCart />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
