345a547143110d283696f23aa8354a12361e1722
manish01101 <manishchotu4@gmail.com>
2024-10-17 15:41:42 +0530

    news section

--- frontend/src/utils/getImgUrl.js
+++ frontend/src/utils/getImgUrl.js
@@ -1,3 +1,6 @@
 export function getImgUrl(name) {
   return new URL(`../assets/books/${name}`, import.meta.url);
 }
+export function getNewsImg(name) {
+  return new URL(`../assets/news/${name}`, import.meta.url);
+}
